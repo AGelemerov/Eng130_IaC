ab30f8acb536b7ab89aa68e4a847e45a83b526bb
agelemerov <angel.gelemerov@gmail.com>
2022-11-18 16:49:40 +0000

    added ASG

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+
 provider "aws" {
   region = "eu-west-1"
 }
@@ -15,6 +16,7 @@ resource "aws_vpc" "eng130-angel-terraform-vpc" {
 resource "aws_subnet" "eng130-angel-terraform-subnet" {
   vpc_id     = aws_vpc.eng130-angel-terraform-vpc.id
   cidr_block = "10.0.6.0/24"
+  availability_zone = "eu-west-1a"
   tags = {
     "Name" = "eng130-angel-terraform-subnet"
   }
@@ -85,9 +87,8 @@ resource "aws_route_table_association" "eng130-angel-terraform-rta" {
   route_table_id = aws_route_table.eng130-angel-terraform-rt.id
 }
 
-
 # Create app instance
-resource "aws_instance" "eng130-angel-terraform-app" {
+resource "aws_instance" "eng130-angel-terraform-controller" {
   ami                         = var.webapp-ami-id # "ami-0b47105e3d7fc023e"
   instance_type               = var.instance_type
   key_name                    = var.key_name
@@ -95,6 +96,47 @@ resource "aws_instance" "eng130-angel-terraform-app" {
   subnet_id                   = aws_subnet.eng130-angel-terraform-subnet.id
   associate_public_ip_address = true
   tags = {
-    Name = "eng130-angel-terraform-app"
+    Name = "eng130-angel-terraform-controller"
+  }
+}
+
+resource "aws_launch_template" "eng130-angel-terraform-lt" {
+  name = "eng130-angel-terraform-lt"
+  image_id = "ami-0eb28d325ae047a58"
+  instance_initiated_shutdown_behavior = "terminate"
+  instance_market_options {
+    market_type = "spot"
+  }
+  instance_type = "t2.micro"
+  key_name = "eng130-new"
+
+  monitoring {
+    enabled = false
+  }
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups = aws_autoscaling_group.eng130-angel-terraform-asg
+  }
+  placement {
+    availability_zone = "eu-west-1"
+  }
+  # vpc_security_group_ids = [aws_security_group.eng130-angel-terraform-sg.id]
+  user_data = filebase64("${path.module}/provision.sh")
+  tags = {
+    "Name" = "eng130-angel-terraform-app"
+  }
+}
+
+resource "aws_autoscaling_group" "eng130-angel-terraform-asg" {
+  name                      = "eng130-angel-terraform-asg"
+  max_size                  = 3
+  min_size                  = 1
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+  desired_capacity          = 1
+  force_delete              = true
+  vpc_zone_identifier       = [aws_subnet.eng130-angel-terraform-subnet.id]
+  launch_template {
+    id = aws_launch_template.eng130-angel-terraform-lt.id
   }
 }
